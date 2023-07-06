7af1d611f53cef9c157cb96811c3557aee127d97
John Denlinger <john_d@dc3.jp>
2023-07-06 11:09:35 +0900

    finish working blog prototype

---
+++ pages/api/post/index.ts
@@ -0,0 +1,15 @@
+import prisma from '../../../lib/prisma';
+
+
+export default async function handle(req, res) {
+  const { title, content, session } = req.body;
+
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
